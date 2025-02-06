d69d1c9042d303bf1d052827dec4afae35ce98b7
angelvz30 <angelvaz3510@gmail.com>
2025-02-06 10:07:55 -0800

    Started the main screen, made the start button and tested the different backgrounds I can use.

--- Flag game/ContentView.swift
+++ Flag game/ContentView.swift
@@ -10,12 +10,19 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            Text("Flag Dash").bold()
+                .font(.largeTitle)
+            Text("Press the button to start").foregroundColor(.gray)
+                }
+                    .padding()
+        
+            Button("Play"){
+                
+                   
+                
+            }
+        
+        
     }
 }
 
