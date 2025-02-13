8084a360595469c6c0f0189d3a78eb110ea4670b
angelvz30 <angelvaz3510@gmail.com>
2025-02-13 10:08:52 -0800

    Fixed the Main screen image and text, and the instructions button.

--- Flag game/ContentView.swift
+++ Flag game/ContentView.swift
@@ -9,37 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Image("earth-background")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-            VStack {
-                Text("Flag Dash")
-                    .font(.largeTitle)
-                    .foregroundColor(.white)
-                    .padding()
-                Spacer()
-                HStack {
-                    ForEach(0..<8, id: \.self) { index in
-                        Image("flag\(index)")
-                            .resizable()
+        @State var showingInstructions = false
+        @State var score = 0
+        NavigationStack{
+            }
+            ZStack {
+                Image("earth-background")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                VStack {
+                    Text("Flag Dash")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                    Spacer()
+                    HStack {
+                        ForEach(0..<8, id: \.self) { index in
+                            Image("flag\(index)")
+                                .resizable()
+                        }
                     }
-                }
-                }
-
                 }
                 
-                Button("Play"){
-                        }
-                Button("Instructions"){
-                    
-        }
-                    }
-                }
+            }
             
-
+            Button("Play") {
+                
+                
+                
+            }
+            Button("Instructions"){
+                showingInstructions = true
+                    NavigationLink(destination: showingInstructions) {
+                            Text("Here is how to play")
+                }
+            }
+        }
+    }
     
-
+    
+    
+    
 
 #Preview {
     ContentView()
